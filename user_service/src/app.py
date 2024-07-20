368cf1fff65cb1aba4fd5fb38ea25c64a5abbc6c
ekiwaka <karan.rawat13@gmail.com>
2024-07-20 12:16:39 +0530

    removed hard-coded db url

--- user_service/src/app.py
+++ user_service/src/app.py
@@ -8,9 +8,9 @@ from functools import wraps
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://user:password@mysql/social_media')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'mysecret')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db.init_app(app)
 
