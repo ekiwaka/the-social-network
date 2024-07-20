3eb8a2e46ce274c57602b4a34bb12dc95ecc26eb
ekiwaka <karan.rawat13@gmail.com>
2024-07-20 10:05:12 +0530

    Added elasticsearch to search APIs and modified API gateway

--- api_gateway/src/app.py
+++ api_gateway/src/app.py
@@ -1,109 +1,36 @@
 from flask import Flask, request, jsonify
 import requests
-import os
 
 app = Flask(__name__)
 
-USER_SERVICE_URL = os.getenv('USER_SERVICE_URL', 'http://user_service:5000')
-DISCUSSION_SERVICE_URL = os.getenv('DISCUSSION_SERVICE_URL', 'http://discussion_service:5000')
-COMMENT_SERVICE_URL = os.getenv('COMMENT_SERVICE_URL', 'http://comment_service:5000')
-LIKE_SERVICE_URL = os.getenv('LIKE_SERVICE_URL', 'http://like_service:5000')
-SEARCH_SERVICE_URL = os.getenv('SEARCH_SERVICE_URL', 'http://search_service:5000')
+USER_SERVICE_URL = 'http://user_service:5001'
+DISCUSSION_SERVICE_URL = 'http://discussion_service:5002'
+COMMENT_SERVICE_URL = 'http://comment_service:5003'
+LIKE_SERVICE_URL = 'http://like_service:5004'
+SEARCH_SERVICE_URL = 'http://search_service:5005'
 
-# User routes
-@app.route('/users', methods=['POST'])
-def create_user():
-    response = requests.post(f'{USER_SERVICE_URL}/users', json=request.get_json())
-    return jsonify(response.json()), response.status_code
+def forward_request(service_url):
+    response = requests.request(
+        method=request.method,
+        url=f"{service_url}{request.path}",
+        headers={key: value for key, value in request.headers if key != 'Host'},
+        params=request.args,
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False
+    )
+    return (response.content, response.status_code, response.headers.items())
 
-@app.route('/users/<user_id>', methods=['PUT'])
-def update_user(user_id):
-    response = requests.put(f'{USER_SERVICE_URL}/users/{user_id}', json=request.get_json())
-    return jsonify(response.json()), response.status_code
-
-@app.route('/users/<user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    response = requests.delete(f'{USER_SERVICE_URL}/users/{user_id}')
-    return jsonify(response.json()), response.status_code
-
-@app.route('/users', methods=['GET'])
-def list_users():
-    response = requests.get(f'{USER_SERVICE_URL}/users')
-    return jsonify(response.json()), response.status_code
-
-@app.route('/users/search', methods=['GET'])
-def search_users():
-    response = requests.get(f'{USER_SERVICE_URL}/users/search', params=request.args)
-    return jsonify(response.json()), response.status_code
-
-# Discussion routes
-@app.route('/discussions', methods=['POST'])
-def create_discussion():
-    response = requests.post(f'{DISCUSSION_SERVICE_URL}/discussions', json=request.get_json())
-    return jsonify(response.json()), response.status_code
-
-@app.route('/discussions/<discussion_id>', methods=['PUT'])
-def update_discussion(discussion_id):
-    response = requests.put(f'{DISCUSSION_SERVICE_URL}/discussions/{discussion_id}', json=request.get_json())
-    return jsonify(response.json()), response.status_code
-
-@app.route('/discussions/<discussion_id>', methods=['DELETE'])
-def delete_discussion(discussion_id):
-    response = requests.delete(f'{DISCUSSION_SERVICE_URL}/discussions/{discussion_id}')
-    return jsonify(response.json()), response.status_code
-
-@app.route('/discussions', methods=['GET'])
-def list_discussions():
-    response = requests.get(f'{DISCUSSION_SERVICE_URL}/discussions')
-    return jsonify(response.json()), response.status_code
-
-@app.route('/discussions/search', methods=['GET'])
-def search_discussions():
-    response = requests.get(f'{DISCUSSION_SERVICE_URL}/discussions/search', params=request.args)
-    return jsonify(response.json()), response.status_code
-
-# Comment routes
-@app.route('/comments', methods=['POST'])
-def create_comment():
-    response = requests.post(f'{COMMENT_SERVICE_URL}/comments', json=request.get_json())
-    return jsonify(response.json()), response.status_code
-
-@app.route('/comments/<comment_id>', methods=['PUT'])
-def update_comment(comment_id):
-    response = requests.put(f'{COMMENT_SERVICE_URL}/comments/{comment_id}', json=request.get_json())
-    return jsonify(response.json()), response.status_code
-
-@app.route('/comments/<comment_id>', methods=['DELETE'])
-def delete_comment(comment_id):
-    response = requests.delete(f'{COMMENT_SERVICE_URL}/comments/{comment_id}')
-    return jsonify(response.json()), response.status_code
-
-@app.route('/comments', methods=['GET'])
-def list_comments():
-    response = requests.get(f'{COMMENT_SERVICE_URL}/comments')
-    return jsonify(response.json()), response.status_code
-
-# Like routes
-@app.route('/likes', methods=['POST'])
-def create_like():
-    response = requests.post(f'{LIKE_SERVICE_URL}/likes', json=request.get_json())
-    return jsonify(response.json()), response.status_code
-
-@app.route('/likes/<like_id>', methods=['DELETE'])
-def delete_like(like_id):
-    response = requests.delete(f'{LIKE_SERVICE_URL}/likes/{like_id}')
-    return jsonify(response.json()), response.status_code
-
-@app.route('/likes', methods=['GET'])
-def list_likes():
-    response = requests.get(f'{LIKE_SERVICE_URL}/likes')
-    return jsonify(response.json()), response.status_code
-
-# Search route
-@app.route('/search', methods=['GET'])
-def search_posts():
-    response = requests.get(f'{SEARCH_SERVICE_URL}/search', params=request.args)
-    return jsonify(response.json()), response.status_code
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(path):
+    if path.startswith('users') or path.startswith('login'):
+        return forward_request(USER_SERVICE_URL)
+    elif path.startswith('discussions'):
+        return forward_request(DISCUSSION_SERVICE_URL)
+    elif path.startswith('comments'):
+        return forward_request(COMMENT_SERVICE_URL)
+    elif path.startswith('likes'):
+        return forward_request(LIKE_SERVICE_URL)
+    elif path.startswith('search'):
+        return forward_request(SEARCH_SERVICE_URL)
+    return jsonify({'message': 'Service not found'}), 404
--- search_service/src/app.py
+++ search_service/src/app.py
@@ -1,15 +1,96 @@
 from flask import Flask, request, jsonify
 from elasticsearch import Elasticsearch
 import os
+import jwt
+from functools import wraps
 
 app = Flask(__name__)
-es = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
 
-@app.route('/search', methods=['GET'])
-def search_posts():
+es = Elasticsearch(os.getenv('ELASTICSEARCH_URL'))
+SECRET_KEY = os.getenv('SECRET_KEY', 'mysecret')
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+
+        try:
+            jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        except Exception as e:
+            return jsonify({'message': 'Token is invalid!', 'error': str(e)}), 403
+
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/search/users', methods=['GET'])
+@token_required
+def search_users():
     query = request.args.get('query')
-    response = es.search(index='discussions', body={'query': {'match': {'hashtags': query}}})
-    return jsonify(response['hits']['hits'])
+    if not query:
+        return jsonify({'message': 'Query parameter is required!'}), 400
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    es_query = {
+        "query": {
+            "multi_match": {
+                "query": query,
+                "fields": ["name", "mobile_no", "email"]
+            }
+        }
+    }
+    
+    try:
+        res = es.search(index="users", body=es_query)
+        users = [{'id': hit['_id'], 'name': hit['_source']['name'], 'mobile_no': hit['_source']['mobile_no'], 'email': hit['_source']['email']} for hit in res['hits']['hits']]
+        return jsonify(users), 200
+    except Exception as e:
+        return jsonify({'message': 'Error executing search query', 'error': str(e)}), 500
+
+@app.route('/search/discussions', methods=['GET'])
+@token_required
+def search_discussions():
+    query = request.args.get('query')
+    if not query:
+        return jsonify({'message': 'Query parameter is required!'}), 400
+
+    es_query = {"query": {"multi_match": {"query": query, "fields": ["text", "hashtags"]}}}
+    
+    try:
+        res = es.search(index="discussions", body=es_query)
+        discussions = [{'id': hit['_id'], 'user_id': hit['_source']['user_id'], 'text': hit['_source']['text'], 'image': hit['_source']['image'], 'created_at': hit['_source']['created_at']} for hit in res['hits']['hits']]
+        return jsonify(discussions), 200
+    except Exception as e:
+        return jsonify({'message': 'Error executing search query', 'error': str(e)}), 500
+
+@app.route('/search/discussions_by_text', methods=['GET'])
+@token_required
+def search_discussions_by_text():
+    text = request.args.get('text')
+    if not text:
+        return jsonify({'message': 'Text parameter is required!'}), 400
+
+    es_query = {"query": {"match": {"text": text}}}
+    
+    try:
+        res = es.search(index="discussions", body=es_query)
+        discussions = [{'id': hit['_id'], 'user_id': hit['_source']['user_id'], 'text': hit['_source']['text'], 'image': hit['_source']['image'], 'created_at': hit['_source']['created_at']} for hit in res['hits']['hits']]
+        return jsonify(discussions), 200
+    except Exception as e:
+        return jsonify({'message': 'Error executing search query', 'error': str(e)}), 500
+
+@app.route('/search/discussions_by_hashtag', methods=['GET'])
+@token_required
+def search_discussions_by_hashtag():
+    hashtag = request.args.get('hashtag')
+    if not hashtag:
+        return jsonify({'message': 'Hashtag parameter is required!'}), 400
+
+    es_query = {"query": {"match": {"hashtags": hashtag}}}
+    
+    try:
+        res = es.search(index="discussions", body=es_query)
+        discussions = [{'id': hit['_id'], 'user_id': hit['_source']['user_id'], 'text': hit['_source']['text'], 'image': hit['_source']['image'], 'created_at': hit['_source']['created_at']} for hit in res['hits']['hits']]
+        return jsonify(discussions), 200
+    except Exception as e:
+        return jsonify({'message': 'Error executing search query', 'error': str(e)}), 500
